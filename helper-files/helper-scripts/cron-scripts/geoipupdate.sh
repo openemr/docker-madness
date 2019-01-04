7fbc118bb9d0479328572eec90711a931382c504
Dan Ehrlich <dan@ehrlichserver.com>
2019-01-04 07:12:53 -0600

    remove geoip

--- helper-files/helper-scripts/cron-scripts/geoipupdate.sh
+++
@@ -1,4 +0,0 @@
-RUN wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz \
-  && tar -xvf GeoLite2-Country* \
-  && rm -f /usr/local/share/GeoIP/GeoLite2-Country.mmdb \
-  && mv GeoLite2-Country*/GeoLite2-Country.mmdb /usr/local/share/GeoIP
--- helper-files/helper-scripts/debian9.sh
+++ helper-files/helper-scripts/debian9.sh
@@ -9,11 +9,11 @@ cd /openemr
 composer install
 npm install --unsafe-perm
 npm run build
-#@todo don't run compose as superuser
+#@todo don't run compose (npm?) as superuser
 composer global require phing/phing
 composer global require phing/phing 
-#/root/.composer/vendor/bin/phing vendor-clean \
-#/root/.composer/vendor/bin/phing assets-clean \
+/root/.composer/vendor/bin/phing vendor-clean \
+/root/.composer/vendor/bin/phing assets-clean \
 composer global remove phing/phing 
 composer dump-autoload -o 
 composer clearcache 
@@ -52,7 +52,7 @@ cp /root/docker-madness/helper-files/config-files/apache/apache2.conf /etc/apach
 cp /root/docker-madness/helper-files/config-files/apache/openemr.conf /etc/apache2/sites-enabled
 #@todo probably eliminate security.conf and just put in main apache2.conf???
 cp /root/docker-madness/helper-files/config-files/apache/security.conf /etc/apache2/conf-enabled
-cp /root/docker-madness/helper-files/config-files/GeoIP.conf /usr/local/etc/
+
 
 ### Load Apache Modules
 #ln -s /etc/apache2/mods-available/socache_smcb.load /etc/apache2/mods-available/socache_smcb.load
@@ -73,7 +73,7 @@ find interface/main/calendar/modules/PostCalendar/pntemplates/compiled -type d -
 find gacl/admin/templates_c -type d -print0 | xargs -0 chmod 700
 
 ### Script Removal
-echo "Removing remaining setup scripts"
+#echo "Removing remaining setup scripts"
 #remove all setup scripts
 #rm -f admin.php
 #rm -f acl_setup.php
@@ -83,4 +83,6 @@ echo "Removing remaining setup scripts"
 #rm -f sql_upgrade.php
 #rm -f ippf_upgrade.php
 #rm -f gacl/setup.php
-echo "Setup scripts removed, we should be ready to go now!"
+#echo "Setup scripts removed, we should be ready to go now!"
+
+apache2ctl -k start
